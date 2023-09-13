ccf14da6a4f6e8e6055bc3e860ab88572e2fd997
Azal <Azal.nj2000@gmail.com>
2023-09-12 17:37:33 -0700

    admin personolized

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ['title', 'author' ,'publish' , 'status']
+    ordering = ['title' , 'publish'] 
+    list_filter = ['status','publish' ,'author']
+    search_fields =['title','description']
+    raw_id_fields =['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug" : ['title']} #automation slug!just for english
+    list_editable = ['status']
+    list_display_links =['author'] #change the hyperrlink in the adminsetting
