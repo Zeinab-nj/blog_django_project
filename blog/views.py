55b28e869939d76c63081186c89000ac558a2b5c
Azal <Azal.nj2000@gmail.com>
2023-09-27 14:43:04 -0700

    url added to blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse , Http404
+from . models import Post 
 
-# Create your views here.
+# Create your views here. 
+
+def index (request) : 
+    return HttpResponse("index")
+
+
+
+def post_list(request):
+    posts = Posts.published.all() 
+    context ={
+        'posts' : posts ,
+    }
+
+    return render(request ,"template1.html",context)
+
+
+def post_details(request, id) :
+    try:
+        posts = Post.published.get(id=id)
+    except:
+        raise Http404("No Posts Found!")
+
+        context ={
+        'post' : post ,
+    }
+
+    return render(request ,"template2.html",context)
+
