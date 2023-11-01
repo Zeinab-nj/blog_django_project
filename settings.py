5e8ccc518787076d047bbc9c79a217ca428f6ed4
Azal <Azal.nj2000@gmail.com>
2023-10-31 23:35:05 -0700

    added Postgresql

--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER' : 'blog_admin'
+        'PASSWORD' : 'Mal1402Mal',
+        'PORT':'5432',
     }
 }
 
